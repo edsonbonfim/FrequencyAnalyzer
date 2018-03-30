97950672b6593198095c48db040682c50db482d2
Edson Onildo <edsononildo@id.uff.br>
2018-03-30 20:43:19 -0300

    Keep housing

--- analisador.c
+++ analisador.c
@@ -1,66 +1,77 @@
+/**
+ * FrequencyAnalyzer
+ *
+ * The purpose of this code is to analyze how often integer
+ * values (consecutive bytes) appear in files of various formats.
+ *
+ * @author   Edson Onildo <inbox.edsononildo@gmail.com>
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define NUM 65535 // 256*256
 
-int *iniciarContador()
+int *startCounter()
 {
     return (int *) calloc(NUM, sizeof(int));
 }
 
-FILE *abrirArquivo(char *filename, char *mode)
+FILE *openFile(char *filename, char *mode)
 {
     FILE *file;
     
     if (!(file = fopen(filename, mode)))
     {
-        perror("Nao foi possivel abrir o arquivo");
+        perror("Could not open file");
         exit(-1);
     }
 
     return file;
 }
 
-void salvar(char *filename, int *contador)
+void saveResponse(char *filename, int *counter)
 {
     int i;
 
-    FILE *file = abrirArquivo(filename, "w+");
+    FILE *file = openFile(filename, "w+");
 
     for (i = 0; i < NUM; i++)
     {
-        fprintf(file, "%d\n", contador[i]);
+        fprintf(file, "%d\n", counter[i]);
     }
 
     fclose(file);
 }
 
-int main()
+int *analyze(char *filename)
 {
-    int i;
-
-    int *contador = iniciarContador();
-
-    char filename[255];
-
-    printf("Arquivo: ");
-    scanf("%s", filename);
-
-    FILE *file = abrirArquivo(filename, "r");
+    FILE *file = openFile(filename, "r");
+    
+    int i, *response = startCounter();
 
     while(!feof(file))
     {
         i = 0;
         fread(&i, 2, 1, file);
-        contador[i]++;
+        response[i]++;
     }
 
     fclose(file);
 
-    printf("Saida: ");
-    scanf("%s", filename);
+    return response;
+}
 
-    salvar(filename, contador);
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("usage: %s <input-file> <output-file>\n", argv[0]);
+        return -1;
+    }
+
+    int *response = analyze(argv[1]);
+    saveResponse(argv[2], response);
 
     return 0;
 }
